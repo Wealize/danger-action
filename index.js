28d396ec7cebbf41c5a22ab10759c98758563a68
Daniel Ranchal Parrado <daniranchal@theneonproject.org>
2020-03-16 15:47:38 +0100

    All should work (#7)
    * All should work
    
    * hmm
    
    * fix
    
    * updating dist

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import * as core from '@actions/core'
 import * as cp from 'child_process'
+import { danger, fail, markdown, message } from 'danger'
+import { istanbulCoverage } from 'danger-plugin-istanbul-coverage'
+import jiraIssue from 'danger-plugin-jira-issue'
+import * as fs from 'fs'
+import * as path from 'path'
 
 async function run() {
   try {
