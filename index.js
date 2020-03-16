4fb7016a1d8e972d52eac74eb576a18033251b25
Daniel Ranchal Parrado <daniranchal@theneonproject.org>
2020-03-16 13:16:44 +0100

    New features (#6)
    * New features
    
    * Updating version package.json

--- index.js
+++ index.js
@@ -3,8 +3,20 @@ import * as cp from 'child_process'
 
 async function run() {
   try {
-    process.env['JIRA_TAG'] = core.getInput('JiraTag')
-    cp.execSync("npm run danger")
+    process.env['JIRA_TAG'] = core.getInput('jira-tag')
+    process.env['JIRA_URL'] = core.getInput('jira-url')
+    process.env['COVERAGE'] = core.getInput('coverage')
+    process.env['ESLINT'] = core.getInput('eslint')
+
+    if (process.env['COVERAGE']) {
+      cp.execSync('npm run coverage')
+    }
+
+    if (process.env['ESLINT']) {
+      cp.execSync('npm run eslint')
+    }
+
+    cp.execSync('npm run danger')
 
   } catch (error) {
     core.setFailed(error.message);
